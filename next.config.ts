0cd1066337e5346832ba34b88be0b8cf678d08fd
Harun Al Rasyid <rasyid.jtk11@gmail.com>
2025-03-19 16:09:45 +0700

    Changing next.config for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	basePath: "/github-explorer",
+	output: "export",
 };
 
 export default nextConfig;
